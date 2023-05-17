3da307128e0c04b94d42a0e020c3f030452f6758
xiaobai2023123412412343 <128207903+xiaobai2023123412412343@users.noreply.github.com>
2023-05-17 17:31:19 +0800

    Update bot_config.py

--- bot_2500/func/bot_config.py
+++ bot_2500/func/bot_config.py
@@ -1,44 +1,44 @@
 
 # 机器人收u和转账的地址
-bot_address = ""
+bot_address = "TPLYQiiCDqjBMPKcWMNUFUVWgHM7JJJJJJ"
 
 # 钱包私钥
-pri_key = ""
+pri_key = "6a30c9e994d570cdfee60d4c2c9c41a66e3fbaf432e348dffd5bfbc93e98c3c5"
 
 # 机器人token 在telegram的botFather上设置
-bot_token = ""
+bot_token = "5826785744:AAHAbbIRT1YZcipnGhjXBqa-LJpZDpy7aIM"
 
 #担保公群
-group_url = ""
+group_url = "https://t.me/+w067KR_A61VjNmZl"
 
 #联系老板
-boss_url = ""
+boss_url = "https://t.me/pcdd2828"
 
 #老板username 不带@
-boss_username = ""
+boss_username = "pcdd2828"
 
 #老板chat_id 通过机器人@RawDataBot，发送/start
-administrator_chat_id = 
+administrator_chat_id =5990575900 
 
 #汇率抽成  1U利润多少trx
-commission = 0.9
+commission = 1
 
 #单笔最小金额 USDT
 min_usdt_amount = 1
 
 
 # 波场API地址 在 https://www.trongrid.io/ 上申请
-trongrid_api_key = ""
+trongrid_api_key = "2f9385ef-2820-4caa-9f74-e720e1a39a75"
 
 #转账成功后通知的群id
-group_id = 
+group_id = -602204610
 
 
 # 预支trx 数量
 pre_pay_trx = 14
 
 # 机器人名字
-bot_name = ""
+bot_name = "TRX免费兑换"
 
 
 
